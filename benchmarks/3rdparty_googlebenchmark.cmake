e31ac505a9b49c94a0ca130f91d2a55e3b334e13
Borys Boiko <burbokop@gmail.com>
2023-08-20 13:08:07 +0300

    Google Benchmarks added

---
+++ benchmarks/3rdparty_googlebenchmark.cmake
@@ -0,0 +1,18 @@
+include(ExternalProject)
+set(GOOGLE_BENCHMARK_INSTALL_PREFIX
+    ${CMAKE_CURRENT_BINARY_DIR}/google_benchmark_install_prefix)
+ExternalProject_Add(
+  GoogleBenchmark
+  URL "https://github.com/google/benchmark/archive/refs/tags/v1.8.2.zip"
+  CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+             -DCMAKE_INSTALL_PREFIX=${GOOGLE_BENCHMARK_INSTALL_PREFIX}
+             -DBENCHMARK_ENABLE_GTEST_TESTS=OFF)
+
+function(target_link_google_benchmark TARGET)
+  add_dependencies(${TARGET} GoogleTest)
+  target_include_directories(${TARGET}
+                             PRIVATE ${GOOGLE_BENCHMARK_INSTALL_PREFIX}/include)
+  target_link_directories(${TARGET} PRIVATE
+                          ${GOOGLE_BENCHMARK_INSTALL_PREFIX}/lib)
+  target_link_libraries(${TARGET} benchmark benchmark_main)
+endfunction()
---
+++ benchmarks/random_gen.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <random>
+
+namespace coschedula::benchmarks::utils {
+
+template<typename T>
+T gen_num()
+{
+    std::random_device rd;
+    std::uniform_int_distribution<T> dist(std::numeric_limits<T>::min(),
+                                          std::numeric_limits<T>::max());
+    return dist(rd);
+}
+
+inline std::string gen_string(std::size_t len)
+{
+    std::string result;
+    result.reserve(len);
+    for (std::size_t i = 0; i < len; ++i) {
+        std::random_device rd;
+        std::uniform_int_distribution<std::string::value_type> dist('a', 'z');
+        result += dist(rd);
+    }
+    return result;
+}
+
+inline std::string gen_text(std::size_t word_len, std::size_t count)
+{
+    std::string result;
+    result.reserve(word_len * count);
+    for (std::size_t i = 0; i < count; ++i) {
+        result += word_len;
+        if (i < count - 1) {
+            result += ' ';
+        }
+    }
+    return result;
+}
+
+} // namespace coschedula::benchmarks::utils
