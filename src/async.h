5a51779128974dde055c9eceaae6eeecd2ef7cd4
Borys Boiko <burbokop@gmail.com>
2024-08-26 00:59:48 +0200

    conschedula::async owerload added to accept another coroutine
    Task: #8

--- src/async.h
+++ src/async.h
@@ -28,4 +28,33 @@ task<T, S> async(auto &&f, Args &&...args)
     co_return future.get();
 }
 
+/**
+ * @brief create thread and runs seperate scheduler in it and current coroutine awaits it
+ */
+template<typename T = void, scheduler S = per_thread_scheduler<default_task_registry>, typename... Args>
+task<T, S> async(auto &&f, Args &&...args)
+    requires requires {
+        {
+            f(std::forward<Args>(args)...)
+        } -> std::same_as<task<T, S>>;
+    }
+{
+    auto future = std::async(
+        std::launch::async,
+        [f = std::move(f)](Args &&...args) {
+            auto task = f(std::forward<Args>(args)...);
+            while (S::proceed()) {
+                std::this_thread::yield();
+            }
+            if constexpr (!std::is_same_v<T, void>) {
+                return task.result().value();
+            }
+        },
+        std::forward<Args>(args)...);
+    while (future.wait_for(std::chrono::seconds(0)) != std::future_status::ready) {
+        co_await suspend{};
+    }
+    co_return future.get();
+}
+
 } // namespace coschedula
